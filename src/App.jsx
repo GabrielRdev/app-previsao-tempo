e2150995da162ddf871aa35be67c2a318852b83a
Gabriel Ricardo <biiel06@hotmail.com>
2024-07-30 16:55:50 -0300

    initial-index-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { useState, useRef } from 'react'
-import './App.css'
+import { useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+import WeatherInformations from "./components/Weatherinformation";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const inputRef = useRef() 
+	const [weather, setWeather] = useState();
+	const inputRef = useRef();
 
-  function searchCity(){
-    const city = inputRef.current.value
-    const key = "07289acbc979d17e39370dc6bb1300c8"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}´
-  }
+	async function searchCity() {
+		const city = inputRef.current.value;
+		const key = "07289acbc979d17e39370dc6bb1300c8";
 
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
 
-  return (
-    <>
-      <div>
-        <h1>DevClub - Previsão do Tempo</h1>
-        <input  ref={inputRef} type='text' placeholder='Digite o nome da cidade' />
-        <button onClick={searchCity}>Buscar</button>
-      </div>
-    </>
-  )
+		const apiInfo = await axios.get(url);
+		setWeather(apiInfo.data);
+	}
+
+	return (
+		<>
+			<div>
+				<h1>DevClub - Previsão do Tempo</h1>
+				<input
+					ref={inputRef}
+					type="text"
+					placeholder="Digite o nome da cidade"
+				/>
+				<button onClick={searchCity}>Buscar</button>
+			</div>
+
+			{weather && <WeatherInformations weather={weather} />}
+		</>
+	);
 }
 
-
-
-export default App
+export default App;
