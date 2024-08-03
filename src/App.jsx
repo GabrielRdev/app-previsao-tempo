f3c590d1b15ff61fa854d742c79b171294ae4390
Gabriel Ricardo <biiel06@hotmail.com>
2024-08-03 14:42:05 -0300

    add-background-color

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,9 @@ function App() {
 				<button onClick={searchCity}>Buscar</button>
 
 				{weather && <WeatherInformations weather={weather} />}
-				{weather5Days && <WeatherInformations5Days weather5Days = {weather5Days} />}
+				{weather5Days && (
+					<WeatherInformations5Days weather5Days={weather5Days} />
+				)}
 			</div>
 		</>
 	);
